0bf3ead9528cf861e25a8de16d4ccaa7059f0f83
wushifeng <wushifengcn@qq.com>
2016-07-04 19:01:21 +0800

    Merge branch 'master' of https://github.com/wushifeng/jpademo
    # Conflicts:
    #	jpa/pom.xml

--- jpa/src/test/java/cn/csbit/BaseTest.java
+++ jpa/src/test/java/cn/csbit/BaseTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by wushifeng on 2016/6/28.
+ * 基础的单元测试类，简化其他单元测试的编写，所有单元测试从这继承
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
